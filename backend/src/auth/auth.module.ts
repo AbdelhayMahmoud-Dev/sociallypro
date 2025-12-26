aac14b57945a34f8c6181d38ac3e41a41a19d1c9
Abdelhay Mahmoud Mohammad <kamreyvg78@gmail.com>
2025-12-26 21:24:09 +0200

    fix: register google strategy in auth module

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -3,18 +3,22 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
-import { PrismaModule } from '../prisma/prisma.module'; 
+import { PrismaModule } from '../prisma/prisma.module';
+import { GoogleStrategy } from './google.strategy'; // تأكد من أن المسار يبدأ بـ g صغير كما سميت الملف
 
 @Module({
   imports: [
     PrismaModule,
     PassportModule,
     JwtModule.register({
-      secret: 'SUPER_SECRET_KEY_123', // مفتاح التشفير
-      signOptions: { expiresIn: '7d' }, // صلاحية 7 أيام
+      secret: 'SUPER_SECRET_KEY_123', // مفتاح التشفير الخاص بك
+      signOptions: { expiresIn: '7d' }, // مدة صلاحية التوكن 7 أيام
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [
+    AuthService, 
+    GoogleStrategy // تم إضافة GoogleStrategy هنا ليتعرف السيرفر على نظام جوجل
+  ],
 })
 export class AuthModule {}
